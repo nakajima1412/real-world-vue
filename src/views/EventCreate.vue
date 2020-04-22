dc9852c79c82bef963ebc9eeea663591a702b7d0
nakajima1412 <nakajima1412@icloud.com>
2020-04-22 22:31:30 +0900

    inputにバリデーションを適用

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -14,19 +14,56 @@
       </template>
 
       <h3>Name & describe your event</h3>
-      <BaseInput label="Title" v-model="event.title" type="text" placeholder="Title" class="field"/>
+      <BaseInput
+        label="Title"
+        type="text"
+        placeholder="Title"
+        class="field"
+        :class="{ error: $v.event.title.$error}"
+        v-model.trim="event.title"
+        @blur="$v.event.title.$touch()"/>
+      <template v-if="$v.event.title.$error">
+        <p v-if="!$v.event.title.required" class="errorMessage">Title is required.</p>
+      </template>
 
-      <BaseInput label="Description" v-model="event.description" type="text" placeholder="Add a description" class="field"/>
+      <BaseInput
+        label="Description"
+        v-model="event.description"
+        type="text"
+        placeholder="Add a description"
+        class="field"
+        :class="{ error: $v.event.description.$error}"
+        @blur="$v.event.description.$touch()"/>
+      <template v-if="$v.event.description.$error">
+        <p v-if="!$v.event.description.required" class="errorMessage">Description is required.</p>
+      </template>
 
       <h3>Where is your event?</h3>
-      <BaseInput label="Location" v-model="event.location" type="text" placeholder="Add a location" class="field"/>
+      <BaseInput
+        label="Location"
+        v-model="event.location"
+        type="text"
+        placeholder="Add a location"
+        class="field"
+        :class="{ error: $v.event.location.$error }"
+        @blur="$v.event.location.$touch()"/>
+      <template v-if="$v.event.location.$error">
+        <p v-if="!$v.event.location.required" class="errorMessage">Location is required.</p>
+      </template>
 
       <h3>When is your event?</h3>
 
       <div class="field">
         <label>Date</label>
-        <datepicker v-model="event.date" placeholder="Select a date"/>
+        <datepicker
+          v-model="event.date"
+          @opened="$v.event.date.$touch()"
+          :input-class="{ error: $v.event.date.$error }"
+          placeholder="Select a date"/>
       </div>
+      <template v-if="$v.event.date.$error">
+        <p v-if="!$v.event.date.required" class="errorMessage">Date is required.</p>
+      </template>
 
       <BaseSelect
         label="Select a time"
@@ -39,7 +76,12 @@
         <p v-if="!$v.event.time.required" class="errorMessage">Time is required.</p>
       </template>
 
-      <BaseButton type="submit" buttonClass="-fill-gradient">Submit</BaseButton>
+      <BaseButton
+        type="submit"
+        buttonClass="-fill-gradient"
+        :disabled="$v.$anyError"
+      >Submit</BaseButton>
+      <p v-if="$v.$anyError" class="errorMessage">Please fill out the required field(s).</p>
     </form>
   </div>
 </template>
@@ -77,6 +119,10 @@ export default {
   },
   methods: {
     createEvent() {
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        return
+      }
       NProgress.start()
       this.$store
         .dispatch('event/createEvent', this.event)
